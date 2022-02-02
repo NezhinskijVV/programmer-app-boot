3a537dfe7d9475b88f6beb61cc0fe15986427414
Vitaliy <SeekerOfConfessor@gmail.com>
2022-02-02 20:43:55 +0300

    add test and yaml file

---
+++ src/test/java/ru/itsjava/services/NotebookServiceImplTest.java
@@ -0,0 +1,35 @@
+package ru.itsjava.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.itsjava.domain.Notebook;
+
+@SpringBootTest
+@DisplayName("Класс NotebookServiceImpl")
+public class NotebookServiceImplTest {
+
+    @Configuration
+    static class MyConfiguration {
+
+        @Bean
+        public NotebookService notebookService() {
+            return new NotebookServiceImpl("apple", "air", 2022);
+        }
+    }
+
+    @Autowired
+    private NotebookServiceImpl notebookService;
+
+    @DisplayName(" должен корректно давать ноутбук")
+    @Test
+    public void shouldHaveCorrectMethodGetNotebook() {
+        Notebook notebook = notebookService.getNotebook();
+        Assertions.assertEquals(notebook.getFirm(), "apple");
+    }
+
+}
